be15c910cd8b7994be3b570c0d11a3b7a0300aa7
vlad777442 <60163112+vlad777442@users.noreply.github.com>
2023-02-22 09:47:33 +0300

    first commit

---
+++ java/src/main/solutions/AddTwoNumbers.java
@@ -0,0 +1,11 @@
+package main.solutions;
+
+//@LeetCode(
+//        id = 2,
+//        name = "Add Two Numbers",
+//        url = "https://leetcode.com/problems/add-two-numbers/",
+//        difficulty = Difficulty.MEDIUM
+//)
+public class AddTwoNumbers {
+
+}
