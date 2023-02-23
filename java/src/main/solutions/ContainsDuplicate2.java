b5e93455f3a846dca4424e0e76f944cc1992e5ab
vlad777442 <60163112+vlad777442@users.noreply.github.com>
2023-02-23 21:28:36 +0300

    added task number

--- java/src/main/solutions/ContainsDuplicate2.java
+++ java/src/main/solutions/ContainsDuplicate2.java
@@ -1,20 +1,35 @@
 package main.solutions;
 
-//@LeetCode(
-//        id = 219,
-//        name = "Contains Duplicate II",
-//        url = "https://leetcode.com/problems/contains-duplicate-ii/",
-//        difficulty = Difficulty.EASY
-//)
+import java.util.HashMap;
+import java.util.Map;
+
+@LeetCode(
+        id = 219,
+        name = "Contains Duplicate II",
+        url = "https://leetcode.com/problems/contains-duplicate-ii/",
+        difficulty = Difficulty.EASY
+)
 public class ContainsDuplicate2 {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-//        int count = 0;
+////        int count = 0;
+//        for (int i = 0; i < nums.length; i++) {
+//            for (int j = i + 1; j < nums.length; j++) {
+//                if (nums[i] == nums[j] && Math.abs(i - j) <= k)
+//                    return true;
+////                    count++;
+//            }
+//        }
+//        return false;
+        if (nums == null || nums.length== 0 || k == 0)
+            return false;
+        Map<Integer, Integer> mymap = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] == nums[j] && Math.abs(i - j) <= k)
+            if (mymap.containsKey(nums[i])) {
+                int prev = mymap.get(nums[i]);
+                if (i - prev <= k)
                     return true;
-//                    count++;
             }
+            mymap.put(nums[i], i);
         }
         return false;
     }
