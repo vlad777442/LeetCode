25a0385070f67ddc519f47da76c21a9914850a72
vlad777442 <60163112+vlad777442@users.noreply.github.com>
2023-03-10 15:19:18 +0300

    solved task 189

---
+++ java/src/main/solutions/RotateArray.java
@@ -0,0 +1,30 @@
+package main.solutions;
+
+import main.common.Difficulty;
+import main.common.LeetCode;
+
+@LeetCode(
+        id = 189,
+        name = "Rotate Array",
+        url = "https://leetcode.com/problems/rotate-array/",
+        difficulty = Difficulty.MEDIUM
+)
+public class RotateArray {
+    public static void main(String[] args) {
+        int[] nums = new int[]{1,2,3,4,5,6,7};
+        rotate(nums, 3);
+
+        for (int i: nums)
+            System.out.println(i);
+    }
+    public static void rotate(int[] nums, int k) {
+        int len = nums.length;
+        int[] res = nums.clone();
+        int j = len - (k % len);
+
+        for (int i = 0; i < len; i++) {
+            nums[i] = res[j % len];
+            j++;
+        }
+    }
+}
