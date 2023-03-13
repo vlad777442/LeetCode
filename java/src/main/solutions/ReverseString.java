6a8709964f3d678dfc1fba7b8eeb6c4fd1361499
vlad777442 <60163112+vlad777442@users.noreply.github.com>
2023-03-13 11:11:20 +0300

    updated task 344

---
+++ java/src/main/solutions/ReverseString.java
@@ -0,0 +1,34 @@
+package main.solutions;
+
+import main.common.Difficulty;
+import main.common.LeetCode;
+
+@LeetCode(
+        id = 344,
+        name = "Reverse String",
+        url = "https://leetcode.com/problems/reverse-string/",
+        difficulty = Difficulty.EASY
+)
+public class ReverseString {
+    public static void main(String[] args) {
+        String s = "hello";
+        char[] c= s.toCharArray();
+        reverseString(c);
+        for (char k: c)
+            System.out.println(k);
+    }
+    public static void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+        char temp;
+
+        while (left < right) {
+            temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+
+    }
+}
