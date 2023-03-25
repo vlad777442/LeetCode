157a3fc68dbad1e0b242fec23abe21bbee6569de
vlad777442 <60163112+vlad777442@users.noreply.github.com>
2023-03-25 17:57:55 +0300

    updated task 3

--- java/src/main/solutions/LongestSubstringWithoutRepeatingCharacters.java
+++ java/src/main/solutions/LongestSubstringWithoutRepeatingCharacters.java
@@ -3,6 +3,8 @@ package main.solutions;
 import main.common.Difficulty;
 import main.common.LeetCode;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 @LeetCode(
         id = 3,
@@ -18,35 +20,50 @@ public class LongestSubstringWithoutRepeatingCharacters {
         System.out.println(lengthOfLongestSubstring(" "));
         System.out.println(lengthOfLongestSubstring("aab"));
         System.out.println(lengthOfLongestSubstring("ohomm"));
+        System.out.println(lengthOfLongestSubstring("abba"));
     }
     public static int lengthOfLongestSubstring(String s) {
         int max = 0;
-        int left;
+        int left = 0;
         int right = 0;
-        ArrayList<Character> characters = new ArrayList<>();
+//        ArrayList<Character> characters = new ArrayList<>();
+        Map<Character, Integer> characters = new HashMap<>();
 
         while (right < s.length()) {
-            if (!characters.contains(s.charAt(right))) {
-                characters.add(s.charAt(right));
+            if (characters.containsKey(s.charAt(right))) {
+                left = Math.max(left, characters.get(s.charAt(right)) + 1);
             }
-            else {
-                left = characters.indexOf(s.charAt(right));
-
-                if (characters.size() > max) {
-                    max = characters.size();
-                }
-                characters.add(s.charAt(right));
-
-                for (int i = 0; i <= left; i++)
-                    characters.remove(0);
-
-            }
+            characters.put(s.charAt(right), right);
+            max = Math.max(max, right - left + 1);
             right++;
         }
-        if (characters.size() > max) {
-            max = characters.size();
-        }
-
         return max;
+//        if (diff > max) {
+//            max = characters.size();
+//        }
+
+//        while (right < s.length()) {
+//            if (!characters.contains(s.charAt(right))) {
+//                characters.add(s.charAt(right));
+//            }
+//            else {
+//                left = characters.indexOf(s.charAt(right));
+//
+//                if (characters.size() > max) {
+//                    max = characters.size();
+//                }
+//                characters.add(s.charAt(right));
+//
+//                for (int i = 0; i <= left; i++)
+//                    characters.remove(0);
+//
+//            }
+//            right++;
+//        }
+//        if (characters.size() > max) {
+//            max = characters.size();
+//        }
+
+
     }
 }
