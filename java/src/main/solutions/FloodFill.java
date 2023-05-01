762c9ad3d328490f80947215d6d574a2dc303c93
vlad777442 <60163112+vlad777442@users.noreply.github.com>
2023-05-01 15:32:44 +0300

    solved task 733

---
+++ java/src/main/solutions/FloodFill.java
@@ -0,0 +1,86 @@
+package main.solutions;
+
+import main.common.Difficulty;
+import main.common.LeetCode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+@LeetCode(
+        id = 733,
+        name = "Flood Fill",
+        url = "https://leetcode.com/problems/flood-fill/",
+        difficulty = Difficulty.EASY
+)
+public class FloodFill {
+    public static void main(String[] args) {
+        int[][] image = {{1,1,1},
+                        {1,1,0},
+                        {1,0,1}};
+        int sr = 1, sc = 1, color = 2;
+        int[][] arr = floodFill(image, sr, sc, color);
+    }
+    public static int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int[][] res = breadthFirstSearch(image, sr, sc, color);
+        for (int i = 0; i < res.length; i++) {
+            for (int j = 0; j < res[0].length; j++)
+                System.out.print(res[i][j] + " ");
+            System.out.println();
+        }
+        return breadthFirstSearch(image, sr, sc, color);
+    }
+
+    public static class Pair {
+        int first, second;
+
+        public Pair(int first, int second) {
+            this.first = first;
+            this.second = second;
+        }
+    }
+
+    public static int[][] breadthFirstSearch(int[][] image, int sr, int sc, int color) {
+        int[] dRow = {-1, 0, 1, 0};
+        int[] dCol = {0, 1, 0, -1};
+        int rows = image.length;
+        int cols = image[0].length;
+        boolean[][] visited = new boolean[rows][cols];
+
+        Queue<Pair> pairs = new LinkedList<>();
+
+        pairs.add(new Pair(sr, sc));
+        visited[sr][sc] = true;
+        int initColor = image[sr][sc];
+        image[sr][sc] = color;
+
+        while (!pairs.isEmpty()) {
+            Pair cell = pairs.peek();
+            int x = cell.first;
+            int y = cell.second;
+
+            pairs.remove();
+
+            for (int i = 0; i < dRow.length; i++) {
+                int adjx = x + dRow[i];
+                int adjy = y + dCol[i];
+
+                if (isVisited(visited, adjx, adjy) && image[adjx][adjy] == initColor) {
+                    pairs.add(new Pair(adjx, adjy));
+                    image[adjx][adjy] = color;
+                    visited[adjx][adjy] = true;
+                }
+            }
+        }
+        return image;
+    }
+
+    public static boolean isVisited(boolean[][] visited, int row, int col) {
+        if (row < 0 || col < 0 ||
+                row >= visited.length || col >= visited[0].length) {
+            return false;
+        }
+        if (visited[row][col])
+            return false;
+        return true;
+    }
+}
